a142410c0b9c25d74a13a59c5edd16c3378a896f
gitzub <142504263+gitzub@users.noreply.github.com>
2023-08-18 14:00:49 +0900

    Delete testpy directory

--- testpy/testpy/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from home import views
-
-urlpatterns = [
-    path('admin', admin.site.urls),
-    path('', views.home),
-]
