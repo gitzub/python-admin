9c6128ea7c7ee5f0255ff6fe36432040676351ae
gitzub <142504263+gitzub@users.noreply.github.com>
2023-08-18 14:03:13 +0900

    Update admin.py

--- testpy/home/admin.py
+++ testpy/home/admin.py
@@ -15,59 +15,6 @@ from .models import User
 # if문 사용을 위해 def방식으로 진행했으나 error
 # 하단 주석처리해둔것 class방식 정상작동
 # # # 에러 def 방식 # # #
-# class MyUserChangeForm(UserChangeForm):
-#     class Meta:
-#         model = User
-#         fields = '__all__'
-
-# class MyUserCreationForm(UserCreationForm):
-#     class Meta:
-#         model = User
-#         fields = '__all__'
-
-# class MyUserAdmin(UserAdmin):
-#     def get_fieldsets(self, request):
-#         if request.user.is_superuser:
-#             fieldsets = (
-#             (None, {'fields': ('username', 'mychoicegroup', 'password')}),
-#             (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-#             )
-#             add_fieldsets = (
-#                 (None, {
-#                     'classes': ('wide',),
-#                     'fields': ('username', 'mychoicegroup', 'password1', 'password2'),
-#                 }),
-#             )
-#             form = MyUserChangeForm
-#             add_form = MyUserCreationForm
-#             list_display = ('username', 'is_staff', 'mychoicegroup')
-#             list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
-#             search_fields = ('username', 'mychoicegroup')
-#             ordering = ('username',)
-#         else:
-#             fieldsets = (
-#                 (None, {'fields': ('username', 'mychoicegroup', 'password')}),
-#                 (_('Permissions'), {'fields': ('groups')}),
-#             )
-#             add_fieldsets = (
-#                 (None, {
-#                     'classes': ('wide',),
-#                     'fields': ('username', 'mychoicegroup', 'password1', 'password2'),
-#                 }),
-#             )
-#             form = MyUserChangeForm
-#             add_form = MyUserCreationForm
-#             list_display = ('username', 'is_staff', 'mychoicegroup')
-#             list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
-#             search_fields = ('username', 'mychoicegroup')
-#             ordering = ('username',)
-#         return fieldsets, add_fieldsets, form, add_form, list_display, list_filter, search_fields, ordering
-
-# admin.site.register(User, MyUserAdmin)
-
-
-
-# # # 정상작동 class 방식 # # #
 class MyUserChangeForm(UserChangeForm):
     class Meta:
         model = User
@@ -79,22 +26,75 @@ class MyUserCreationForm(UserCreationForm):
         fields = '__all__'
 
 class MyUserAdmin(UserAdmin):
-    fieldsets = (
-        (None, {'fields': ('username', 'password', 'mychoicegroup')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                      'groups', 'user_permissions')}),
-        )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'mychoicegroup', 'password1', 'password2'),
-            }),
-        )
-    form = MyUserChangeForm
-    add_form = MyUserCreationForm
-    list_display = ('username', 'mychoicegroup', 'is_staff')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups', 'mychoicegroup')
-    search_fields = ('username', 'mychoicegroup')
-    ordering = ('username',)
+    def get_fieldsets(self, request):
+        if request.user.is_superuser:
+            fieldsets = (
+            (None, {'fields': ('username', 'mychoicegroup', 'password')}),
+            (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+            )
+            add_fieldsets = (
+                (None, {
+                    'classes': ('wide',),
+                    'fields': ('username', 'mychoicegroup', 'password1', 'password2'),
+                }),
+            )
+            form = MyUserChangeForm
+            add_form = MyUserCreationForm
+            list_display = ('username', 'is_staff', 'mychoicegroup')
+            list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+            search_fields = ('username', 'mychoicegroup')
+            ordering = ('username',)
+        else:
+            fieldsets = (
+                (None, {'fields': ('username', 'mychoicegroup', 'password')}),
+                (_('Permissions'), {'fields': ('groups')}),
+            )
+            add_fieldsets = (
+                (None, {
+                    'classes': ('wide',),
+                    'fields': ('username', 'mychoicegroup', 'password1', 'password2'),
+                }),
+            )
+            form = MyUserChangeForm
+            add_form = MyUserCreationForm
+            list_display = ('username', 'is_staff', 'mychoicegroup')
+            list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+            search_fields = ('username', 'mychoicegroup')
+            ordering = ('username',)
+        return fieldsets, add_fieldsets, form, add_form, list_display, list_filter, search_fields, ordering
 
 admin.site.register(User, MyUserAdmin)
+
+
+
+# # # 정상작동 class 방식 # # #
+# class MyUserChangeForm(UserChangeForm):
+#     class Meta:
+#         model = User
+#         fields = '__all__'
+
+# class MyUserCreationForm(UserCreationForm):
+#     class Meta:
+#         model = User
+#         fields = '__all__'
+
+# class MyUserAdmin(UserAdmin):
+#     fieldsets = (
+#         (None, {'fields': ('username', 'password', 'mychoicegroup')}),
+#         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+#                                       'groups', 'user_permissions')}),
+#         )
+#     add_fieldsets = (
+#         (None, {
+#             'classes': ('wide',),
+#             'fields': ('username', 'mychoicegroup', 'password1', 'password2'),
+#             }),
+#         )
+#     form = MyUserChangeForm
+#     add_form = MyUserCreationForm
+#     list_display = ('username', 'mychoicegroup', 'is_staff')
+#     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups', 'mychoicegroup')
+#     search_fields = ('username', 'mychoicegroup')
+#     ordering = ('username',)
+
+# admin.site.register(User, MyUserAdmin)
